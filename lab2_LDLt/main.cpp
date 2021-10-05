b979c8a36d61859bf7424acd4745716086ec111d
ShabunyaPolina <Shabunya.polina@mail.ru>
2021-10-05 19:53:30 +0300

    finish (hope so)

--- lab2_LDLt/main.cpp
+++ lab2_LDLt/main.cpp
@@ -3,12 +3,13 @@
 
 #include <iostream>
 #include <vector>
-
+#include <ctime>
+#include <cmath>
 
 // ввод матрицы
 void fill_matrix(std::vector<std::vector<float>> &a) {
     int n = 0;
-    std::cout << "n: ";
+    std::cout << "n = ";
     std::cin >> n;
     a.assign(n, std::vector<float>(n));
     for(int i = 0; i < n; ++i) {
@@ -30,6 +31,18 @@ void show_matrix(std::vector<std::vector<float>> &a) {
     }
 }
 
+// вывод расширенной матрицы СЛАУ
+void show_system(std::vector<std::vector<float>> &a, std::vector<float> &b) {
+    int n = (int)a.size();
+    for(int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << a[i][j] << '\t';
+        }
+        std::cout << "|\t" << b[i] << '\n';
+    }
+}
+
+// LDLt разложение матрицы
 void decompose(std::vector<std::vector<float>> &a) {
     int n = (int)a.size();
     std::vector<float> tmp(n*n);
@@ -44,6 +57,9 @@ void decompose(std::vector<std::vector<float>> &a) {
     }
 }
 
+// ФУНКЦИИ ДЛЯ ПРОВЕРКИ РАЗЛОЖЕНИЯ
+
+// выделяет матрицу L
 void toL(std::vector<std::vector<float>> &a, std::vector<std::vector<float>> &l) {
     int n = (int)a.size();
     l.assign(n, std::vector<float>(n));
@@ -57,6 +73,7 @@ void toL(std::vector<std::vector<float>> &a, std::vector<std::vector<float>> &l)
     }
 }
 
+// выделяет матрицу D
 void toD(std::vector<std::vector<float>> &a, std::vector<std::vector<float>> &d) {
     int n = (int)a.size();
     d.assign(n, std::vector<float>(n));
@@ -65,6 +82,7 @@ void toD(std::vector<std::vector<float>> &a, std::vector<std::vector<float>> &d)
     }
 }
 
+// транспонирует матрицу
 void transpose(std::vector<std::vector<float>> &x) {
     int n = (int)x.size();
     for(int i = 0; i < n; ++i) {
@@ -74,6 +92,7 @@ void transpose(std::vector<std::vector<float>> &x) {
     }
 }
 
+// перемножает матрицы
 void multiply(std::vector<std::vector<float>> &a,
               std::vector<std::vector<float>> &b,
               std::vector<std::vector<float>> &res) {
@@ -88,29 +107,144 @@ void multiply(std::vector<std::vector<float>> &a,
     }
 }
 
+// генерирует матрицу А размерности n*n
+void generate_matrix(std::vector<std::vector<float>> &a, int n) {
+    a.assign(n, std::vector<float>(n));
+    srand ( time(nullptr) );
+    const int MIN_RAND = -4;
+    const int MAX_RAND = 0;
+    // генерация недиагональных элементов матрицы А
+    for(int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            a[i][j] = a[j][i] = MIN_RAND + rand() % (MAX_RAND - MIN_RAND + 1);
+        }
+    }
+    // вычисление диагональных элементов матрицы А
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            if(j == i) continue;
+            a[i][i] -= a[i][j];
+        }
+    }
+}
+
+// вычисляет a[0][0] в зависимости от k
+void set_a00(std::vector<std::vector<float>> &a, int k) {
+    a[0][0] += (float)pow(10, -1 * k);
+}
+
+// вычисляет столбец b СЛАУ
+void calculate_b(std::vector<std::vector<float>> &a,
+                 std::vector<float> &b,
+                 std::vector<float> &x, int m) {
+    // генерация вектора X
+    int n = (int)a.size();
+    x.resize(n);
+    b.resize(n);
+    for(int i = 0; i < n; ++i) {
+        x[i] = (float)(m + i);
+    }
+    // вычисление столбца b
+    for(int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            b[i] += a[i][j] * x[j];
+        }
+    }
+}
+
+// решает систему
+void solve_system(std::vector<std::vector<float>> &a,
+                  std::vector<float> &b,
+                  std::vector<float> &x) {
+    int n = (int)a.size();
+    x.resize(n);
+    std::vector<float> tmp(n);
+    // решение системы L*tmp=b, где tmp = DLt
+    for(int i = 0; i < n; ++i) {
+        tmp[i] = b[i];
+        for(int j = 0; j < i; ++j) {
+            tmp[i] -= a[i][j] * tmp[j];
+        }
+    }
+    // решение системы DLt*x=tmp
+    for(int i = n - 1; i >= 0; --i) {
+        x[i] = tmp[i] / a[i][i];
+        for(int j = n - 1; j > i; --j) {
+            x[i] -= a[j][i] * x[j];
+        }
+    }
+}
+
+// вычисляет относительную погрешность в кубической норме
+float calculate_error(std::vector<float> &x, std::vector<float> &true_x) {
+    float tmp = 0, max_delta = 0, max_true_x = 0;
+    for(int i = 0; i < x.size(); ++i) {
+        tmp = std::fabs(x[i] - true_x[i]);
+        if(tmp > max_delta)
+            max_delta = tmp;
+        if(std::fabs(x[i]) > max_true_x)
+            max_true_x = std::fabs(x[i]);
+    }
+    return max_delta / max_true_x;
+}
+
 int main() {
-    std::vector<std::vector<float>> a{{4,2,3},{2,4,1},{3,1,3}};
-    std::vector<std::vector<float>> l;
-    std::vector<std::vector<float>> lt;
-    std::vector<std::vector<float>> d;
-    std::vector<std::vector<float>> res1;
-    std::vector<std::vector<float>> res2;
-    //fill_matrix(a);
+    std::vector<std::vector<float>> a;
+    std::vector<std::vector<float>> a1;
+    std::vector<float> b;
+    std::vector<float> b1;
+    std::vector<float> x;
+    std::vector<float> x1;
+    std::vector<float> x2;
+
+    generate_matrix(a, 12);
+    a1 = a;
+
+    set_a00(a,0);
+    set_a00(a1,1);
+    calculate_b(a, b, x, 10);
+    calculate_b(a1, b1, x, 10);
+    std::cout << "СЛАУ:\n";
+    std::cout << "\nk = 0\n";
+    show_system(a, b);
+    std::cout << "\nk = 1\n";
+    show_system(a1, b1);
+    std::cout << "\nПреобразованная матрица А (нижний треугольник - L "
+                 "(за исключением главной диагонали), диагональ - D):\n";
+
+    std::cout << "\nk = 0\n";
     decompose(a);
-    toL(a,l);
-    lt = l;
     show_matrix(a);
-    std::cout << "\nL:\n";
-    show_matrix(l);
-    transpose(lt);
-    std::cout << "\nLt:\n";
-    show_matrix(lt);
-    toD(a, d);
-    std::cout << "\nD:\n";
-    show_matrix(d);
-    std::cout << "\n";
-    multiply(l,d, res1);
-    multiply(res1, lt, res2);
-    show_matrix(res2);
+    std::cout << "\nk = 1\n";
+    decompose(a1);
+    show_matrix(a1);
+
+    solve_system(a,b,x1);
+    solve_system(a,b,x2);
+
+    std::cout << "\nВектор решений систем:\n";
+    for(auto &i : x) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "\nВекторы решений системы, полученный решением системы на основе LDLt разложения:";
+    std::cout << "\nk = 0\n";
+    for(auto &i : x1) {
+        std::cout << i << ' ';
+    }
+    std::cout << "\nk = 1\n";
+    for(auto &i : x2) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+
+    float er1 = 0, er2 = 0;
+    er1 = calculate_error(x1, x);
+    er2 = calculate_error(x2, x);
+    std::cout << "\nПогрещность метода:\n";
+    std::cout << "k = 0:  " << er1;
+    std::cout << "\nk = 1:  " << er2;
+    
     return 0;
 }
